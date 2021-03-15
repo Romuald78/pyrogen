2886e36e9bc1bc0f815f2abd9fcd64e67e0b576f
Romuald GRIGNON <romuald.grignon@gmail.com>
2021-03-15 13:27:07 +0100

    [RELEASE] v0.0.7.dev3 now the full build is working just remains to do it : so do it !

--- __init__.py
+++ __init__.py
@@ -3,28 +3,28 @@
 from .version import __version__
 
 # Main
-#from .ecs.core.main.scene  import Scene
+from .ecs.core.main.scene  import Scene
 from .ecs.core.main.entity import Entity
 
 # Components
-#from .ecs.core.components.gfx       import GfxAnimatedSprite
+from .ecs.core.components.gfx       import GfxAnimatedSprite
 from .ecs.core.components.input     import Keyboard, GamepadButton, GamepadAxis, MouseButton, MouseMotion
 from .ecs.core.components.script    import Script
-#from .ecs.core.components.physic    import PhysicBox, PhysicDisc, PhysicCollision
-#from .ecs.core.components.light     import LightFx
-#from .ecs.core.components.sfx       import Music
+from .ecs.core.components.physic    import PhysicBox, PhysicDisc, PhysicCollision
+from .ecs.core.components.light     import LightFx
+from .ecs.core.components.sfx       import Music
 from .ecs.core.components.transform import Transform
 from .ecs.core.components.camera    import Camera
 from .ecs.core.components.idle      import Idle
 
 # Systems
-#from .ecs.core.systems.sceneSystem  import SceneSystem
-#from .ecs.core.systems.gfxSystem    import GfxSystem
+from .ecs.core.systems.sceneSystem  import SceneSystem
+from .ecs.core.systems.gfxSystem    import GfxSystem
 from .ecs.core.systems.inputSystem  import InputSystem
 from .ecs.core.systems.scriptSystem import ScriptSystem
-#from .ecs.core.systems.physicSystem import PhysicSystem
-#from .ecs.core.systems.lightSystem  import LightSystem
-#from .ecs.core.systems.musicSystem  import MusicSystem
+from .ecs.core.systems.physicSystem import PhysicSystem
+from .ecs.core.systems.lightSystem  import LightSystem
+from .ecs.core.systems.musicSystem  import MusicSystem
 from .ecs.core.systems.cameraSystem import CameraSystem
 from .ecs.core.systems.idleSystem   import IdleSystem
 
