c9cabe738d7531e0702ae2bb8c84878d579cbbe4
Romuald GRIGNON <romuald.grignon@gmail.com>
2021-03-16 13:23:18 +0100

    [RELEASE] v.1.0 First release with working simple demo

--- demo.py
+++ demo.py
@@ -1,7 +1,12 @@
 import sys
-
+import pathlib
+import os
 from .tutorials.rundemo import runDemo
 
 if len(sys.argv)>1:
+
+    curPath = pathlib.Path(__file__).parent.absolute()
+    os.chdir(f"{curPath}/tutorials/")
+
     runDemo(sys.argv[1])
 
--- version.py
+++ version.py
@@ -1,3 +1,3 @@
 # Version of the pyrogen package
 # Used both in the code AND in the setuptools config file
-__version__ = "0.0.11"
+__version__ = "0.1.0"
